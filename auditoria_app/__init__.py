dd9bf4bd30633772694b892e6f825ec170348fc8
Luciano Lino Pereira <enf.lucianolino@gmail.com>
2025-05-26 10:32:44 -0300

    fix: instala wkhtmltopdf e adapta caminho para ambiente Linux

--- auditoria_app/__init__.py
+++ auditoria_app/__init__.py
@@ -48,13 +48,9 @@ def create_app():
         return getattr(obj, attr)
 
 
-    # Configura wkhtmltopdf com base no sistema operacional
     if platform.system() == "Windows":
-        path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
-    else:
-        path_wkhtmltopdf = '/usr/bin/wkhtmltopdf'
+    path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+else:
+    path_wkhtmltopdf = '/usr/local/bin/wkhtmltopdf'
 
-    # Salva a configuração no app (opcional, mas útil)
-    app.config['PDFKIT_CONFIG'] = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
-
-    return app
+pdfkit_config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
